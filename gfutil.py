84487a9f9c1c895ca1cb71982356ace0f8b98e13
Eric Hopper <hopper@omnifarious.org>
2016-04-30 22:13:27 -0400

    Add comments with some nice primitive polynomials.

--- gfutil.py
+++ gfutil.py
@@ -1,4 +1,24 @@
 # <gf.gfMeta(5, (1, 4, 1, 4, 1, 4, 1))((0, 0, 0, 0, 1, 3))>
+# (%i58) p2;p3;p4;p5;p6;p7;
+# Evaluation took 0.0000 seconds (0.0000 elapsed)
+#                                      15
+# (%o58)                              x   - x
+# Evaluation took 0.0000 seconds (0.0000 elapsed)
+#                          6    5    4      3      2
+# (%o59)                  x  + x  + x  + 2 x  + 4 x  + 3
+# Evaluation took 0.0000 seconds (0.0000 elapsed)
+#                             6    5      2
+# (%o60)                     x  + x  + 2 x  + 4 x + 4
+# Evaluation took 0.0000 seconds (0.0000 elapsed)
+#                       6      5    4      3    2
+# (%o61)               x  + 4 x  + x  + 4 x  + x  + 4 x + 2
+# Evaluation took 0.0000 seconds (0.0000 elapsed)
+#                            6      5      4      2
+# (%o62)                    x  + 4 x  + 4 x  + 4 x  + 1
+# Evaluation took 0.0000 seconds (0.0000 elapsed)
+#                       6    5    4      3      2
+# (%o63)               x  + x  + x  + 3 x  + 3 x  + 3 x + 4
+
 
 def gen_groups(l, prime):
     size = len(l)
